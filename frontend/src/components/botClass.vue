bb4d7383930ff8659123c59ae714efe981cf715b
Metallicus Dev <>
2020-12-08 22:27:47 -0400

    upgrade to new data structure

--- frontend/src/components/botClass.vue
+++ frontend/src/components/botClass.vue
@@ -7,11 +7,15 @@
     <!-- Mobile dropdown -->
     <div class="sm:hidden">
       <label for="tabs" class="sr-only">Select a tab</label>
-      <select id="tabs" name="tabs" class="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+      <select
+        id="tabs"
+        name="tabs"
+        class="block w-full focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
+        @change="tierChange($event)"
+      >
         <option
           v-for="tier of tiers"
           :key="tier"
-          :selected="tier === selectedTier"
         >
           {{ tier }}
         </option>
@@ -25,7 +29,7 @@
           <a
             v-for="(tier, i) of tiers"
             :key="tier"
-            @click="() => selectTier(tier, i)"
+            @click="selectTier(i)"
             class="w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm cursor-pointer"
             :class="{
               'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': tier !== selectedTier,
@@ -50,9 +54,13 @@ export default {
     }
   },
   methods: {
-    selectTier (tier, i) {
-      this.selectedTier = tier
+    selectTier (i) {
+      this.selectedTier = this.tiers[i]
       this.$emit('select-bot-index', i)
+    },
+
+    tierChange (e) {
+      this.selectTier(e.target.options.selectedIndex)
     }
   }
 }
