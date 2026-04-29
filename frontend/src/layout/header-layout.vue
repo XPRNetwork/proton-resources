ffb67839911ab4bf59ea084e28392fe3590a7a8c
Andrey Akulov <andreyjamer@users.noreply.github.com>
2026-04-30 01:02:11 +0500

    XPR-237: Fixed icons (#10)
    * XPR-237: Frontend migrated to Vue 3.5+, Pinia and Vite.
    
    * XPR-237: Fixed icons. Improved table of the faucets page

--- frontend/src/layout/header-layout.vue
+++ frontend/src/layout/header-layout.vue
@@ -9,7 +9,7 @@
           <img class="w-auto h-8 sm:h-10" src="@/assets/logo.svg" alt="" />
         </router-link>
 
-        <nav class="flex space-x-4 sm:space-x-10 justify-center pr-2">
+        <nav class="flex gap-4 md:gap-6 lg:gap-10 justify-center pr-2">
           <router-link to="/" class="text-base font-medium text-gray-500 hover:text-purple-900">
             Resources
           </router-link>
@@ -23,7 +23,7 @@
             to="/create-account"
             class="text-base font-medium text-gray-500 hover:text-purple-900"
           >
-            <span class="hidden sm:inline-block">Create</span> Account
+            <span class="hidden md:inline-block">Create</span> Account
           </router-link>
           <!-- <router-link to="/bots" class="hidden sm:block text-base font-medium text-gray-500 hover:text-purple-900">
             Bots
@@ -40,6 +40,17 @@
             class="hidden sm:block text-base font-medium text-gray-500 hover:text-purple-900"
           >
             Github
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3 w-3 inline-block align-super"
+              viewBox="0 0 640 640"
+            >
+              <!-- !Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc. -->
+              <path
+                d="M384 64c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L265.3 329.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L512 173.3V256c0 17.7 14.3 32 32 32s32-14.3 32-32V96c0-17.7-14.3-32-32-32zm-240 96c-44.2 0-80 35.8-80 80v256c0 44.2 35.8 80 80 80h256c44.2 0 80-35.8 80-80v-80c0-17.7-14.3-32-32-32s-32 14.3-32 32v80c0 8.8-7.2 16-16 16H144c-8.8 0-16-7.2-16-16V240c0-8.8 7.2-16 16-16h80c17.7 0 32-14.3 32-32s-14.3-32-32-32z"
+                fill="currentColor"
+              />
+            </svg>
           </a>
         </nav>
 
